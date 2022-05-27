db1f635565198b53ff793faf0676068da3acb706
SebastianL <sebastian.lesniewski.edu@gmail.com>
2022-05-28 01:08:33 +0200

    fixed ecb

--- ecb.py
+++ ecb.py
@@ -1,5 +1,5 @@
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
-from image import *
+from image_loader import *
 import os
 
 
@@ -22,28 +22,28 @@ class ECB:
         return result
 
     @staticmethod
-    def run_encryption(file):
+    def run_encryption(file, error):
         # Load
-        img = Image()
+        img = ImageLoader()
         img.load_file(file)
-        img_v = img.to_vector()
+        img_v = img.simulate_error(file, error)
         # Encrypt
         ecb = ECB()
         enc_v = ecb.encrypt(img_v)
-        img_enc = Image()
+        img_enc = ImageLoader()
         img_enc.from_vector(enc_v, img.img)
         img_enc.save_file(filename_encrypted(file))
-        Secret.save_secret(ecb.key, file)
+        Secret.save_secret(ecb.key, filename_encrypted(file))
 
     @staticmethod
-    def run_decryption(file):
+    def run_decryption(file, error):
         # Load
-        img = Image()
-        img.load_file(filename_encrypted(file))
-        img_v = img.to_vector()
+        img = ImageLoader()
+        img.load_file(file)
+        img_v = img.simulate_error(file, error)
         # Decrypt
         ecb = ECB(Secret.load_secret(file))
         dec_v = ecb.decrypt(img_v)
-        img_dec = Image()
+        img_dec = ImageLoader()
         img_dec.from_vector(dec_v, img.img)
         img_dec.save_file(filename_decrypted(file))
